d7bdce33c2e1041065f061a6a3ada026a6c50db1
JansenLefever <jansenlefever@gmail.com>
2021-03-08 01:47:22 +0000

    updated version. Added truncate command

--- ops05.sh
+++ ops05.sh
@@ -9,25 +9,38 @@
 cat  /var/log/syslog 
 cat /var/log/wtmp
 
+input=0
+
 #Askes user for input
-echo
-echo "Do you want to shred syslong and wtmp: yes or no"
-read input
+while [ $input != 4 ] 
+    do
 
+    echo
+    echo "What would you like to do with the Syslog and wtmp 1) Truncate 2) Shred 3) Print Syslog and wtmp 4) Exit"
+    read input
+
+    if [ $input -eq 1 ]; then
+    sudo truncate -s 0 /var/log/syslog 
+    sudo truncate -s 0 /var/log/wtmp
+    fi
 #Checks in input is what they want to do
-if [ $input == yes ]; then
+    if [ $input -eq 2 ]; then
 
-#Shreds logs
-sudo shred -vfvu /var/log/syslog
-sudo shred -vfvu /var/log/wtmp
+    #Shreds logs
+    sudo shred -vfvu /var/log/syslog
+    sudo shred -vfvu /var/log/wtmp
+    fi
 
-#Shows what is left in the files
-cat /var/log/syslog 
-cat /var/log/wtmp
+    #Shows what is left in the files
+    if [ $input -eq 3 ]; then
+    cat /var/log/syslog 
+    cat /var/log/wtmp
+    fi
 
-echo
-echo "done"
-fi
+    echo
+    echo "done"
+    echo
+done
 
 #End
 
