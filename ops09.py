49b81196bff304191c88feb7471a736176b90bcb
JansenLefever <jansenlefever@gmail.com>
2021-03-11 23:23:15 +0000

    Augmenting a list in python

---
+++ ops09.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+
+# Author: Jansen Lefever
+# Class: 301D2 Ops Challenge 6
+# Purpose: Augmenting a list in python
+
+thislist = ["apple", "banana", "cherry", "blueberry", "cranberry", "celery", "avocado", "pears", "pinnapple", "peach"]
+
+thislist[6] = 'onion'
+print(thislist[3])
+print(thislist[5:9])
+
+
+# https://www.statology.org/replace-values-in-list-python/
