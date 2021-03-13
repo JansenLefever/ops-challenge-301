1592786d38ffb60e3561d631f953aa3363033734
JansenLefever <jansenlefever@gmail.com>
2021-03-13 03:59:46 +0000

    python conditional statements

---
+++ ops10.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python3
+
+# Author: Jansen Lefever
+# Class: 301D2 Ops Challenge 6
+# Purpose:
+
+a = 22
+
+b = 21
+
+
+if a == b:
+  print("A and B are equal")
+elif a != b:
+  print("A and B are not eqaul")
+
+
+if a < b:
+  print("A is Less than B")
+elif a <= b:
+  print("A is less or equal to B")
+else:
+  print("and")
+  
+
+if a > b:
+  print("A is greater than B")
+elif a >= b:
+  print("A is greater or equal to B") 
+else:
+  print("Done")
+
